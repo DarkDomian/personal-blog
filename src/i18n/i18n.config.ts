492765630a6f8e127cd18f578c9334b4e465f575
DarkDomian <darkdomian@gmail.com>
2024-11-22 21:35:51 +0300

    i18n update

--- src/i18n/i18n.config.ts
+++ src/i18n/i18n.config.ts
@@ -2,7 +2,7 @@ import { Config } from 'next-i18n-router/dist/types';
 
 const i18nConfig: Config = {
     locales: ['en', 'ru', 'ja', 'nb'],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
   };
-  
+
 export default i18nConfig;
